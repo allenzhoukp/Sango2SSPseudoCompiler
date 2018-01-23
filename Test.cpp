696350db578e676aa782dfda879dba76ab48fb0b
allenzhoukp <zhoukp.15@sem.tsinghua.edu.cn>
2018-01-24 00:28:22 +0800

    Revert "Cancel all backward argument pushing"
    This reverts commit ecda82504c48aa9db02224250b00fdf63acbda26.

---
+++ Test.cpp
@@ -0,0 +1,8 @@
+void function Magic116 () callsign 710 {
+
+    var obj = CreateObjectBelongTo(GetINV(INTV_ATTACKER_MAJOR), 44001, 0, 0);
+    SetObjectFriction (obj, -1);
+
+}
+
+#include "magic.cpp"
